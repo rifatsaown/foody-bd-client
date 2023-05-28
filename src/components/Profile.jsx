5ec4f00ece4b7d139e738a46c81813e1b8cce589
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-28 11:59:30 +0600

    Login done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 
-
 const Profile = () => {
-    const {user} = useContext(AuthContext);
-    const {displayName, photoURL} = user;
+  const { user } = useContext(AuthContext);
+  const { displayName, photoURL } = user;
   return (
-    <div className="pt-72">
+    <div className="pt-24">
       <div className="avatar">
         <div className="w-24 mask mask-hexagon">
           <img src={photoURL} />
         </div>
       </div>
+      <h1 className="text-2xl font-bold">{displayName}</h1>
     </div>
   );
 };
